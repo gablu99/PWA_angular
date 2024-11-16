ea5f3a7031e59fbcdff56d0babd0c439252f1222
gablu99 <sukdebpstucse@gmail.com>
2024-11-17 00:03:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,20 @@ if ('serviceWorker' in navigator) {
   	console.log('ServiceWorker registration failed: ', err);
 	});
   });
+
+	@Component({
+  selector: 'PWA',
+  templateUrl: './PWA.index.html',
+  styleUrls: ['./PWA.style.css']
+})
+export class SquareCalculatorComponent {
+  number: number | null = null; // User input
+  result: number | null = null; // Result of the calculation
+
+  calculateSquare() {
+    if (this.number !== null) {
+      this.result = this.number * this.number;
+    }
+  }
+}
 }
